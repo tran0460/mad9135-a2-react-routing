18235e96588612572adac7ea9e3297f80becccaf
tran0460 <tran0460@algonquinlive.com>
2022-10-10 16:27:37 -0400

    create routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <Routes>
+        <Route path="/">
+          <Route index element={<></>} />
+          <Route path="home" element={<></>} />
+          <Route path="hourly" element={<></>} />
+          <Route path="daily" element={<></>} />
+          <Route path="*" element={<></>} />
+        </Route>
+      </Routes>
+    </>
+  );
 }
 
 export default App;
