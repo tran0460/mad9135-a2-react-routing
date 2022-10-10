398c7cf0904dceaa93744a8fe4f812a9986dfbf6
tran0460 <tran0460@algonquinlive.com>
2022-10-10 17:00:39 -0400

    import components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
+import MainScreen from "./screens/MainScreen/MainScreen";
+import HomeScreen from "./screens/HomeScreen/HomeScreen";
+import HourlyScreen from "./screens/HourlyScreen/HourlyScreen";
+import DailyScreen from "./screens/DailyScreen/DailyScreen";
+import ErrorScreen from "./screens/ErrorScreen/ErrorScreen";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/">
-          <Route index element={<></>} />
-          <Route path="home" element={<></>} />
-          <Route path="hourly" element={<></>} />
-          <Route path="daily" element={<></>} />
-          <Route path="*" element={<></>} />
+        <Route path="/" element={<MainScreen />}>
+          <Route index path="home" element={<HomeScreen />} />
+          <Route path="hourly" element={<HourlyScreen />} />
+          <Route path="daily" element={<DailyScreen />} />
+          <Route path="*" element={<ErrorScreen />} />
         </Route>
       </Routes>
     </>
