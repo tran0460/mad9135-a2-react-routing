7aa1ac0b196d5352c21d45042c1065d97e86ca9a
tran0460 <tran0460@algonquinlive.com>
2022-10-10 18:25:23 -0400

    create fetch data function

--- src/screens/MainScreen/MainScreen.jsx
+++ src/screens/MainScreen/MainScreen.jsx
@@ -1,16 +1,46 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "../../components/Navbar/Navbar";
 import Searchbar from "../../components/Searchbar/Searchbar";
+import { getGeolocation } from "../../utils/map.service";
+import { getForecast } from "../../utils/weather.service";
 import "./MainScreen.css";
 
 const MainScreen = () => {
-  let [query, setQuery] = useState("");
+  const [query, setQuery] = useState("");
+  const [location, setLocation] = useState({});
+  const [weatherData, setWeatherData] = useState({});
+  const handleSearch = async () => {
+    const respObj = await getGeolocation(query);
+    setLocation(respObj);
+  };
+
+  const getWeatherData = async () => {
+    const respObj = await getForecast({
+      coord: {
+        lat: location.lat,
+        lon: location.lon,
+      },
+      units: "metric",
+    });
+    setWeatherData(respObj);
+  };
+  useEffect(() => {
+    if (!location.lat || !location.lon) return;
+    getWeatherData();
+  }, [location]);
+  useEffect(() => {
+    console.log(weatherData);
+  }, [weatherData]);
   return (
     <div>
       <Navbar />
       <div className="container main-container">
-        <Searchbar query={query} setQuery={setQuery} />
+        <Searchbar
+          query={query}
+          setQuery={setQuery}
+          onSearch={() => handleSearch()}
+        />
         <Outlet />
       </div>
     </div>
