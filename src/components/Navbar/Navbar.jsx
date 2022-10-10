fce8913c0ea6c4acf29ce2649b18bd886c476a66
tran0460 <tran0460@algonquinlive.com>
2022-10-10 17:45:17 -0400

    style navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./Navbar.css";
 
 const Navbar = () => {
   return (
-    <div>
+    <div className="navbar container">
       <h1>RWA</h1>
-      <nav>
+      <nav className="nav-container">
         <Link to="home">Home</Link>
         <Link to="daily">Daily</Link>
         <Link to="hourly">Hourly</Link>
