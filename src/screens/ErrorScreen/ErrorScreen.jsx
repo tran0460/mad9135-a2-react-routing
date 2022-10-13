9c2059db0dd30476897519ec362ea3f0c4736766
tran0460 <tran0460@algonquinlive.com>
2022-10-13 13:15:12 -0400

    style 404 page

--- src/screens/ErrorScreen/ErrorScreen.jsx
+++ src/screens/ErrorScreen/ErrorScreen.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import "./ErrorScreen.css";
 
 const ErrorScreen = () => {
-  return <div>ErrorScreen</div>;
+  return (
+    <div className="error-screen-container">
+      <p className="error-text">404</p>
+      <p>Page not found</p>
+    </div>
+  );
 };
 
 export default ErrorScreen;
