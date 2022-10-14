e54fbd9a0f28a0ae4afade4a0afd04266be5c505
tran0460 <tran0460@algonquinlive.com>
2022-10-14 18:55:05 -0400

    change browser router to hash router

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter basename="/mad9135-a2-react-routing">
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 );
