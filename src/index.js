e14186342609a73ba13bb3d76442f60d1ec9d117
tran0460 <tran0460@algonquinlive.com>
2022-10-14 18:33:38 -0400

    add basename

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename="/mad9135-a2-react-routing">
     <App />
   </BrowserRouter>
 );
